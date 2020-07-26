1e575f1c037c97437356bcc760a60e1db008589f
Eduard Stanculet <estanculet@gmail.com>
2020-07-26 15:45:14 +0300

    Fraud Alers with Median Finder

---
+++ heaps/FraudAlerts/FraudAlerts.cs
@@ -0,0 +1,94 @@
+﻿using heaps.MinMaxHeap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace heaps.FraudAlerts
+{
+    public class FraudAlerts
+    {
+        internal static ITestOutputHelper output;
+
+        public static int MedianFinder(int[] items)
+        {
+            var minHeap = new MinHeap<int>();
+            var maxHeap = new MaxHeap<int>();
+
+            foreach (int item in items)
+            {
+                maxHeap.Add(item);
+                if (maxHeap.Length > items.Length / 2)
+                {
+                    var maxItem = maxHeap.Pop(0);
+                    minHeap.Add(maxItem);
+                }
+            }
+
+            return items.Length % 2 == 1 ?
+                minHeap[0] : ((minHeap[0] + maxHeap[0]) / 2);
+        }
+
+        public static int activityNotificationsTrivial(int[] expenditure, int d)
+        {
+            int n = expenditure.Length;
+            int counter = 0;
+            for (int i = d; i < n; i++)
+            {
+                var past = expenditure.Skip(i - d).Take(d).OrderBy(x => x).ToArray();
+                var median = d % 2 == 1 ? past[d / 2] * 0.1 : ((past[d / 2] * 0.1 + past[d / 2] * 0.1 - 1) * 0.1 / 2);
+                if (expenditure[i] >= median)
+                    counter++;
+                output.WriteLine($"{counter}");
+            }
+            return counter;
+        }
+        public static int activityNotifications(int[] expenditure, int d)
+        {
+            var minHeap = new MinHeap<int>();
+            var maxHeap = new MaxHeap<int>();
+
+            for (int i = 0; i < d; i++)
+            {
+                maxHeap.Add(expenditure[i]);
+                if (maxHeap.Length > (d + 1) / 2)
+                {
+                    var maxItem = maxHeap.Pop(0);
+                    minHeap.Add(maxItem);
+                }
+            }
+
+            int counter = 0;
+            for (int i = d; i < expenditure.Length; i++)
+            {
+                var mid = d % 2 == 1 ? (2 * maxHeap[0]) : (minHeap[0] + maxHeap[0]);
+                output.WriteLine($"{mid}");
+                if (expenditure[i] >= mid) counter++;
+
+                var oldExpenditure = expenditure[i - d];
+                if (oldExpenditure <= maxHeap[0])
+                    maxHeap.PopItem(oldExpenditure);
+                else
+                    minHeap.PopItem(oldExpenditure);
+
+                maxHeap.Add(expenditure[i]);
+                if (maxHeap.Length > (d + 1) / 2)
+                {
+                    var maxItem = maxHeap.Pop(0);
+                    minHeap.Add(maxItem);
+                }
+                else if (maxHeap[0] > minHeap[0])
+                {
+                    var maxMax = maxHeap.Pop(0);
+                    var minMin = minHeap.Pop(0);
+                    maxHeap.Add(minMin);
+                    minHeap.Add(maxMax);
+                }
+
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ heaps/FraudAlerts/FraudAlertsTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace heaps.FraudAlerts
+{
+    public class FraudAlertsTests
+    {
+        public FraudAlertsTests(ITestOutputHelper output)
+        {
+            FraudAlerts.output = output;
+        }
+
+
+        [Theory]
+        [InlineData(@"E:\funwithflags\heaps\FraudAlerts\input01.txt", 633)]
+        [InlineData("E:\\funwithflags\\heaps\\FraudAlerts\\input05.txt", 0)]
+        [InlineData("E:\\funwithflags\\heaps\\FraudAlerts\\input07.txt", 1)]
+        public void TestCase1(string inFile, int expectedResult)
+        {
+            var lines = File.ReadAllLines(inFile);
+
+            string[] nd = lines[0].Split(' ');
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] expenditure = Array.ConvertAll(lines[1].Split(' '), 
+                expenditureTemp => Convert.ToInt32(expenditureTemp))
+            ;
+
+            int result = FraudAlerts.activityNotifications(expenditure, d);
+            Assert.Equal(expectedResult, result);
+
+            Assert.Equal(
+                2,
+                FraudAlerts.activityNotifications(new[] { 2, 3, 4, 2, 3, 6, 8, 4, 5 }, 5)
+                );
+        }
+
+        public static object[][] MidFinderTests = new object[][]
+        {
+            new object[] { new int[] { 0 }, 0 },
+            new object[] { new int[] { 1 }, 1 },
+            new object[] { new int[] { 1, 3 }, 2 },
+            new object[] { new int[] { 1, 3, 4 }, 3 },
+            new object[] { new int[] { 1, 3, 4, 5, 1, 1 }, 2 },
+            new object[] { new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, 2 },
+            new object[] { new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 3 }, 2 },
+            new object[] { new int[] { 2, 2, 2, 2, 2, 2, 3, 3, 3 }, 2 },
+            new object[] { new int[] { 3, 3, 3, 2, 2, 2, 3, 3, 3 }, 3 },
+            new object[] { new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 }, 2 }
+        };
+        [Theory]
+        [MemberData(nameof(MidFinderTests))]
+        public void MidFinder(int[] input, int mid)
+        {
+            int foundMid = FraudAlerts.MedianFinder(input);
+            Assert.Equal(mid, foundMid);
+        }
+
+    }
+}
